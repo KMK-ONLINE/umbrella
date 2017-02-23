574781a7bc9ceb01b1a78c3c95a496d24beeb4bb
William <pair+william@kmkonline.co.id>
2017-02-23 14:17:06 -0500

    Add check for ansible 'block'

--- python/taglint.py
+++ python/taglint.py
@@ -17,15 +17,23 @@ def complain(s):
     err = 1
 
 
+def check_item(filename, item):
+    if 'block' in item:
+        for i in item['block']:
+            check_item(filename, i)
+        return
+
+    if ('tags' not in item) and ('include' not in item):
+        complain('%s: tags not in %s' % (filename, repr(item)))
+
 def run_lint(filename, stream):
     objs = yaml.safe_load(stream)
     if not isinstance(objs, list):
         complain('%s: top level object not a list' % (filename,))
         return
-        
+
     for i in objs:
-        if ('tags' not in i) and ('include' not in i):
-            complain('%s: tags not in %s' % (filename, repr(i)))
+        check_item(filename, i)
 
 
 def main(argv):
@@ -36,7 +44,7 @@ def main(argv):
             with open(fn, 'r') as f:
                 run_lint(fn, f)
     sys.exit(err)
-            
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
